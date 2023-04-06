785b10e19eec6ff5f02b5eab54f10e70d88ba046
ideamoves-sys <ideamoves@gmail.com>
2023-04-06 23:24:25 +0530

    code

---
+++ src/pages/About.js
@@ -0,0 +1,41 @@
+import { Route, useNavigate } from "react-router-dom";
+//useHistory
+
+// nested routes
+import Offers from "./Offers";
+
+export default function About() {
+  // const history = useHistory();
+  const navigate = useNavigate();
+  return (
+    <div className="content">
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+        nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+        eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+        numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+        doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+        Recusandae, ad!
+      </p>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+        nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+        eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+        numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+        doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+        Recusandae, ad!
+      </p>
+
+      {/* <button onClick={() => history.push("/product")}>See Products</button> */}
+      <button onClick={() => navigate("/products")}>See Products</button>
+
+      {/* <Route path="/about/offers">
+        <Offers />
+      </Route> */}
+    </div>
+  );
+}
